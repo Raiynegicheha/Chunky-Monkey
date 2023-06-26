b0ec7d6cbf619bc9bfc9d0845ecdbeb0644b449e
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2023-06-26 17:11:31 +0300

    Update recursive.js

--- recursive.js
+++ recursive.js
@@ -5,11 +5,12 @@ function chunkArrayInGroups(arr, size) {
   function newChunks (arr, size, mArr){
     if (arr.length > 0 ){
       mArr.push(arr.splice(0, size));
+      //splice alters the main array removing starting at index 0, 2 elements.
       newChunks(arr, size, mArr);
     }
 
   }
-  newChunks(arr, size, mArr);
+  newChunks(arr, size, mArr); //calling the function.
   return mArr;
 }
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
